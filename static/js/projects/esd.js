e9aeb58809aed2f85442cd68e830a82e528b9dab
DiegoLHendrix <diegolhendrix@gmail.com>
2025-01-02 13:12:58 -0500

    Edited project cards, landing page links, and javascript

--- static/js/projects/esd.js
+++ static/js/projects/esd.js
@@ -3,7 +3,17 @@ const filterModal = new bootstrap.Modal('#filterModal', {
     keyboard: false
 });
 
-// Show the Filter modal when the button is clicked
+// Show the Filter modal when the button is clicked0
 document.getElementById('showfilterModalButton').addEventListener('click', () => {
     filterModal.show();
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const modalToOpen = urlParams.get('modal');
+
+    if (modalToOpen === 'filter') {
+        const filterModal = new bootstrap.Modal(document.getElementById('filterModal'));
+        filterModal.show();
+    }
 });
--- static/js/projects/evt.js
+++ static/js/projects/evt.js
@@ -13,7 +13,20 @@ const lvssModal = new bootstrap.Modal('#lvssModal', {
     keyboard: false
 });
 
-// Show the LVSS modal when the button is clicked
+// Show the LVSS or CAN modal when the button is clicked
 document.getElementById('showLvssModalButton').addEventListener('click', () => {
     lvssModal.show();
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const modalToOpen = urlParams.get('modal');
+
+    if (modalToOpen === 'lvss') {
+        const lvssModal = new bootstrap.Modal(document.getElementById('lvssModal'));
+        lvssModal.show();
+    } else if (modalToOpen === 'can') {
+        const canModal = new bootstrap.Modal(document.getElementById('canModal'));
+        canModal.show();
+    }
 });
