86e49258337b0891ec219e4bdd2dab80418a7466
DiegoLHendrix <diegolhendrix@gmail.com>
2025-04-04 11:40:55 -0400

    Added a loader, optimized site by using lazy react method

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,47 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 // Utilities
 import { DynamicTitle } from "./components/base/DynamicTitle.ts";
+import SpinnerLoader from "./components/base/SpinnerLoader.jsx";
 
 // Components
 import NavigationBar from "./components/base/NavigationBar.jsx";
 import Footer from "./components/base/Footer.jsx";
-import Home from "./pages/base/Home.jsx";
-import Resume from "./pages/base/Resume.jsx";
-import Contact from "./pages/base/Contact.jsx";
-import NotFound from "./pages/base/NotFound.jsx";
-import Projects from "./pages/projects/projects.jsx";
-import Experience from "./pages/projects/Experience.jsx";
+
+const Home = lazy(() => import("./pages/base/Home.jsx"));
+const Resume = lazy(() => import("./pages/base/Resume.jsx"));
+const Contact = lazy(() => import("./pages/base/Contact.jsx"));
+const NotFound = lazy(() => import("./pages/base/NotFound.jsx"));
+const Projects = lazy(() => import("./pages/projects/projects.jsx"));
+const Experience = lazy(() => import("./pages/projects/Experience.jsx"));
 
 function App() {
   return (
-    <div className="page-container">
-      {" "}
-      {/* Flexbox Container */}
-      <NavigationBar />
-      <div className="content-wrap">
-        {" "}
-        {/* Ensures main content expands */}
-        <BrowserRouter>
-          <DynamicTitle /> {/* This will update the title dynamically */}
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/resume" element={<Resume />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/experience" element={<Experience />} />
-            {/* Catch-all route for undefined pages */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+    <BrowserRouter>
+      <div className="page-container">
+        {/* Flexbox Container */}
+        <NavigationBar />
+        <div className="content-wrap">
+          <Suspense fallback={<SpinnerLoader />}>
+            {/* Ensures main content expands */}
+            <DynamicTitle /> {/* This will update the title dynamically */}
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/resume" element={<Resume />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/experience" element={<Experience />} />
+              {/* Catch-all route for undefined pages */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
